2a826a5686ee9bcd27d6e348c171e2c5c4a3115c
Oskar Niesen <oskarniesen1@gmail.com>
2024-12-01 13:04:59 -0600

    Removed extra build directory

--- Game/build/CMakeFiles/Game.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Game.dir/main.cpp.o: \
- /home/oskar/iowastateuniversity/GameEngineProject/Game/main.cpp \
- /usr/include/stdc-predef.h \
- /home/oskar/iowastateuniversity/GameEngineProject/GameEngine/include/GameEngine/library.h
--- GameEngine/build/CMakeFiles/GameEngine.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libGameEngine.a"
-  "libGameEngine.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
