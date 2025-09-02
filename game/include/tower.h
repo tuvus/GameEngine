d96d6311545ee1ff5fbaaa403ef8da02f487670e
Oskar Niesen <oskarniesen1@gmail.com>
2025-09-01 20:25:04 -0500

    Added early return to tower.h

--- game/include/tower.h
+++ game/include/tower.h
@@ -19,35 +19,37 @@ class Tower : public Game_Object {
     }
 
     void Update() override {
-        if (reload == 0) {
-            Vector2 home = Vector2(team == 0 ? game_manager.application.screen_height : 0,
-                                   game_manager.application.screen_width / 2);
-            Unit* closest_unit = nullptr;
-            float dist = INT_MAX;
-            for (auto* object : game_manager.Get_All_Objects()) {
-                if (Unit* other = dynamic_cast<Unit*>(object)) {
-                    if (other->team == team || !other->spawned)
-                        continue;
-                    if (Vector2Distance(pos, other->pos) > range)
-                        continue;
-                    float new_dist = Vector2Distance(other->pos, home);
-                    if (new_dist >= dist)
-                        continue;
-
-                    closest_unit = other;
-                    dist = new_dist;
-                }
-            }
-
-            if (closest_unit != nullptr) {
-                // Fire
-                closest_unit->spawned = false;
-                game_manager.Delete_Object(closest_unit);
-                reload = 100;
-                rot = Get_Rotation_From_Positions(pos, closest_unit->pos);
-            }
-        } else {
+        if (reload != 0) {
             reload--;
+            return;
         }
+
+        Vector2 home = Vector2(team == 0 ? game_manager.application.screen_height : 0,
+                               game_manager.application.screen_width / 2);
+        Unit* closest_unit = nullptr;
+        float dist = INT_MAX;
+        for (auto* object : game_manager.Get_All_Objects()) {
+            if (Unit* other = dynamic_cast<Unit*>(object)) {
+                if (other->team == team || !other->spawned)
+                    continue;
+                if (Vector2Distance(pos, other->pos) > range)
+                    continue;
+                float new_dist = Vector2Distance(other->pos, home);
+                if (new_dist >= dist)
+                    continue;
+
+                closest_unit = other;
+                dist = new_dist;
+            }
+        }
+
+        if (closest_unit == nullptr)
+            return;
+
+        // Fire
+        closest_unit->spawned = false;
+        game_manager.Delete_Object(closest_unit);
+        reload = 100;
+        rot = Get_Rotation_From_Positions(pos, closest_unit->pos);
     }
 };
